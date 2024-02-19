214457111e305b2ee7aec8ebaaa6f73e1fd4d501
IyengarShruti <iyengarshrutii@gmail.com>
2024-02-19 12:07:36 -0500

    Initial commit

---
+++ src/main/java/net/javafiles/mybanking/MybankingApplication.java
@@ -0,0 +1,13 @@
+package net.javafiles.mybanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javafiles/mybanking/MybankingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javafiles.mybanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
